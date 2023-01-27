1ec93c42f85fb80e89bf619cc92040e65198903b
Vitalii Boiko <boiko_ak18@nuwm.edu.ua>
2023-01-27 22:31:17 +0200

    testing unity physics

---
+++ Assets/_Scripts/Car/InputControllerSystem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public struct InputControllerSystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+
+    }
+
+    public void OnDestroy(ref SystemState state)
+    {
+
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+
+    }
+
+}
